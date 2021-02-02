3704e1fdbc26c1d6644c4ca6612d873d5d073523
jbrasko <jbrasko0508@gmail.com>
2021-02-01 20:43:34 -0600

    commit tree ceda51d3b8b56644e5e0e4379e584e199c4ce854

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadImages();
+});
+
+function loadImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => {
+        json.message.forEach(image => addImage(image))
+    });
+}
+
+function addImage(image) {
+    let container = document.getElementById('dog-image-container')
+    let newImageEl = document.createElement('img')
+    newImageEl.src = image
+    container.appendChild(newImageEl)
+}
